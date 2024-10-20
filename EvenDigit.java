1fa33f51396e21f263e32fb4d1e83181741c5f2d
Divyakamal-004 <kamaldivya650@gmail.com>
2024-10-20 21:12:23 +0530

    find the number of numbers in an array having even digits.

---
+++ EvenDigit.java
@@ -0,0 +1,37 @@
+public class EvenDigit {
+    public static void main(String[] args) {
+       int[] nums={12,345,2,6,7896};
+       System.out.println(findNumbers(nums));
+       System.out.println(digits(234566));
+    }
+    static int findNumbers(int[] nums){
+        int count=0;
+        for(int num: nums){
+            if(even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    //function to check whether a number contains even digits or not
+    static boolean even(int num){
+
+        int numofdigits=digits(num);
+        if(numofdigits%2==0){
+            return true;
+        }
+        return false;
+    }
+    //count number of digits in a number
+    static int digits(int num){
+        if(num<0){
+            num=num*-1;
+        }
+        int count=0;
+        while(num>0){
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
+}
